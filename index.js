759212c274fe79eb2791a1f2595995a08ea6ecdd
Saul Felipe Ramirez <safe.ramirez@gmail.com>
2019-06-10 22:28:29 -0500

    add express server

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
+const express = require('express')
+const app = express()
 
-(async () => {
+const main = async () => {
   const browser = await puppeteer.launch({
     headless: true
   });
   const page = await browser.newPage();
   await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36")
-  await page.goto('https://web.whatsapp.com');
+  await page.goto('https://web.whatsapp.com')
   await page.screenshot({
     path: 'screenshot.png'
   });
 
-  await browser.close();
-})();
+  await browser.close()
+}
+
+app.get("/", (req, res) => {
+  res.send("Hola mundo")
+})
+
+app.listen(3000, () => {
+  console.log("http://localhost:3000")
+})
