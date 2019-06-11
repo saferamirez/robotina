beb376916a82fb8050ac7cad13d61a60182a2bcd
Saul Felipe Ramirez <safe.ramirez@gmail.com>
2019-06-10 22:10:11 -0500

    add agent to script

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const puppeteer = require('puppeteer');
     headless: true
   });
   const page = await browser.newPage();
+  await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36")
   await page.goto('https://web.whatsapp.com');
   await page.screenshot({
     path: 'screenshot.png'
